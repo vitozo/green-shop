955942b370eaec8b654dd4f17120881ccb54be1a
Nomad-lab-sketch <52406767+Nomad-lab-sketch@users.noreply.github.com>
2021-12-02 19:17:43 +0300

    tsx

---
+++ src/components/Header/Heder.tsx
@@ -0,0 +1,12 @@
+import headerLogo from "../../images/icons/header-logo.svg"
+import React from "react";
+
+const Header:React.FC = (props) => {
+    return(
+        <div>
+            <img src={headerLogo} alt="Error"/>
+        </div>
+    )
+}
+
+export default Header
