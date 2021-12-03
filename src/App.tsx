ed5d33729ecca46557a5641e987bed383f56eceb
Vitaliy Tkachenko <vitozov9@gmail.com>
2021-12-03 17:50:33 +0200

    Add and change header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import s from "./App.module.css"
-import Header from "./components/Header/Heder";
+import Header from "./Components/Header/Heder";
+
 
 
 
 const App:React.FC = () => {
   return (
-    <div className={s.body}>
+    <div className={s.container}>
       <Header/>
     </div>
   );
--- src/components/Header/Heder.tsx
+++ src/components/Header/Heder.tsx
@@ -1,10 +1,54 @@
-import headerLogo from "../../images/icons/header-logo.svg"
 import React from "react";
+import {NavLink} from "react-router-dom";
+import s from './css/Header.module.css';
 
-const Header:React.FC = (props) => {
-    return(
-        <div>
-            <img src={headerLogo} alt="Error"/>
+import headerLogo from "../../images/icons/header-logo.svg";
+import headerSearch from "../../images/icons/header-search.svg";
+import headerCart from "../../images/icons/header-cart.svg";
+import headerLogin from "../../images/icons/header-login.svg";
+
+const Header: React.FC = (props) => {
+    return (
+        <div className={s.header}>
+            <NavLink to={''} className={s.header__logo}>
+                <img src={headerLogo} alt="Header logo" className={s.header__logoimg}/>
+            </NavLink>
+            <nav className={s.header__nav}>
+                <ul className={s.header__list}>
+                    <li className={s.header__item}>
+                        <NavLink to={''} className={s.header__item_link}>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li className={s.header__item}>
+                        <NavLink to={''} className={s.header__item_link}>
+                            Shop
+                        </NavLink>
+                    </li>
+                    <li className={s.header__item}>
+                        <NavLink to={''} className={s.header__item_link}>
+                            Plant Care
+                        </NavLink>
+                    </li>
+                    <li className={s.header__item}>
+                        <NavLink to={''} className={s.header__item_link}>
+                            Blogs
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <div className={s.header__actions}>
+                <NavLink to={''} className={`${s.header__actions_icon} ${s.header__search}`}>
+                    <img src={headerSearch} alt=""/>
+                </NavLink>
+                <NavLink to={''} className={`${s.header__actions_icon} ${s.header__cart} `}>
+                    <img src={headerCart} alt=""/>
+                </NavLink>
+                <NavLink to={''} className={`${s.header__actions_icon} ${s.header__login}`}>
+                    <img src={headerLogin} className={s.header__login_img} alt=""/>
+                    Login
+                </NavLink>
+            </div>
         </div>
     )
 }
