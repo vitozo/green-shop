4efe366879cc542b9f822f74639e8191b0704fe6
Nomad-lab-sketch <52406767+Nomad-lab-sketch@users.noreply.github.com>
2021-12-06 20:21:50 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import s from "./App.module.css"
-import Header from "./Components/Header/Heder";
-import Footer from "./Components/Footer/Footer";
-import InfoForm from "./Components/InfoForm/InfoForm";
-import Contacts from "./Components/Contacts/Contacts";
+
+import Header from "./components/Header/Heder";
+import InfoForm from "./components/InfoForm/InfoForm";
+import Contacts from "./components/Contacts/Contacts";
+import Footer from "./components/Footer/Footer";
+
 
 
 
